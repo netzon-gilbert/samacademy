e7f9fcb9df97eb3b577fa1da98c58bd4fe698423
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-11 18:07:45 +0800

    exercise 5: js function to compute value of b to power of n

---
+++ js/exercises/Exer 5/exer.js
@@ -0,0 +1,11 @@
+// TODO: Write your solution here out;
+function run() {
+    // TODO: Start calling your function here
+    var input1 = document.getElementById('input1').value,
+        input2 = document.getElementById('input2').value,
+        output = document.getElementById('output');
+
+        output.innerHTML = Math.pow(input1, input2);
+
+    console.log('Code Awesome');
+};
