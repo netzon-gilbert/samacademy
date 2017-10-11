f560e8f0acd47113f5a99bb352bae8f10a55e7d7
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-11 18:13:43 +0800

    exercise 8: js program to get the width and height of the window on resize

---
+++ js/exercises/Exer 8/exer.js
@@ -0,0 +1,11 @@
+// TODO: Write your solution here
+var ouput = document.getElementById('output');
+
+window.addEventListener('resize', function () {
+    output.innerHTML = window.innerWidth + ' px, ' + window.innerHeight + ' px';
+});
+
+function run () {
+    // TODO: Start calling your function here
+    console.log('Code Awesome');
+};
