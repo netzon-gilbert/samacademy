977241c7ab967c867546a500c28f59d743da2338
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-11 18:12:50 +0800

    exercise 7: adding the forgotten js file

---
+++ js/exercises/Exer 7/exer.js
@@ -0,0 +1,11 @@
+// TODO: Write your solution here
+var images = ['assets/image1.jpg', 'assets/image2.jpg', 'assets/image3.jpg',
+            'assets/image4.jpg', 'assets/image5.jpg'];
+
+function run() {
+    // TODO: Start calling your function here
+    var rand = Math.round(Math.random() * 10) % 5;
+
+    document.getElementById('display').src = images[rand];
+    console.log('Code Awesome');
+};
