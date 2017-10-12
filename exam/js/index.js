d0187ef5c3a56cad07d74a6fb9e1b49120e2bf42
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-12 17:06:05 +0800

    calculator exam : created calculator class and successfully access the input and store to the calculator object

--- exam/js/index.js
+++ exam/js/index.js
@@ -1,4 +1,47 @@
-//alert("good");
+var CalculatorClass = function () {
+
+    this.cart = [];
+    this.inputValues = [];
+
+    this.init = function () {
+        this.cart['price'] = document.getElementById('price');
+        this.cart['income'] = document.getElementById('yeld');
+        this.cart['type'] = document.getElementById('type');
+        this.cart['term'] = document.getElementById('term');
+        this.fetchInput();
+    };
+
+    this.fetchInput = function () {
+        this.inputValues['price'] = this.cart['price'].value;
+        this.inputValues['income'] = this.cart['income'].options[this.cart['income'].selectedIndex].value;
+        this.inputValues['type'] = this.cart['type'].options[this.cart['type'].selectedIndex].value;
+        this.inputValues['term'] = this.cart['term'].options[this.cart['term'].selectedIndex].value;
+    };
+
+    this.display = function () {
+        this.fetchInput();
+        alert(
+            this.inputValues['price'] + "" +
+            this.inputValues['income'] + "" +
+            this.inputValues['type'] + "" +
+            this.inputValues['term']
+            );
+    };
+
+    this.render = function () {
+
+    };
+};
+
+var calculator = new CalculatorClass();
+
+window.onload = function () {
+    calculator.init();
+    alert('good');
+};
+
 function shortTerm() {
-    alert("Comming soon.");
+    alert('Comming soon.');
+    calculator.display();
 }
+
