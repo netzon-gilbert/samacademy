7fb33813421149736ecfcdb5bb769a5c30392059
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-16 14:31:40 +0800

    donera application: trying to access the ranges input values and the output elements for the display;

--- donera/js/index.js
+++ donera/js/index.js
@@ -14,7 +14,6 @@ var SemiCircleClass = function (id, label, value, color) {
         canv.rotate(2 * rotation * Math.PI);
         canv.arc(0, 0, 73, 0, percent * 2 * Math.PI);
         canv.stroke();
-        console.log(percent);
         canv.restore();
 
     };
@@ -28,17 +27,17 @@ var RangesClass = function () {
         rangesWindow = document.getElementById('app-controls');
     };
 
-    this.add = function (id, label, value) {
-        rangesWindow.innerHTML += '<div class="ctr-range" id="">'+
-                                '<span>' + value + '</span>' +
-                                '<label>' + label + '</label>'+
-                                '<input value="'+ value +
-                                '" type="range" id="" class="range-box" />'+
-                                '<input type="button" class="close-btn" value="x" /></div>';
+    this.clear = function () {
+        rangesWindow.innerHTML = '';
     };
 
-    this.remove = function () {
-
+    this.add = function (id, label, value, color) {
+        rangesWindow.innerHTML += '<div class="ctr-range" id="range' + id + '">' +
+                                '<span>' + value + '</span>' +
+                                '<label>' + label + '</label>' +
+                                '<input value="' + value  + '" type="range" style="' +
+                                '" id="" class="range-box" />' +
+                                '<input type="button" class="close-btn" value="x" /></div>';
     };
 
     this.setEvent = function () {
@@ -94,15 +93,27 @@ var AppClass = function () {
         for(var i = 0; i < ranges.length; i++) {
             ranges[i].draw(canv, ranges[i].value / total, rotation);
             rotation += ranges[i].value / total;
+            this.getValues(i);
+
         }
     };
 
+    this.getValues = function (nput) {
+        ranges[nput].value = document.getElementById('range' + nput).childNodes[0].value;
+        console.log(ranges[nput].value);
+    }
     this.erase = function () {
         canv.clearRect(0, 0, 200, 200);
     };
 
     this.addControl = function (label, value, color) {
         ranges.push(new SemiCircleClass(1, label, value, color));
+
+        this.controls.clear();
+        for (var i = 0; i < ranges.length; i++) {
+            this.controls.add(i, ranges[i].label , ranges[i].value, ranges[i].color);
+
+        }
     };
 
     this.removeControl = function (nput) {
@@ -119,11 +130,11 @@ var AppClass = function () {
         this.draw();
     };
 };
+
 var app = new AppClass();
 
 window.onload = function () {
 
-    console.log('good');
     app.init();
 
     setInterval(function () {
@@ -140,9 +151,8 @@ function add() {
     if (value !== null && value <= 100 && value >= 0) {
 
         app.addControl(label, value, color);
-        app.controls.add(1, label, value);
 
     } else {
-        alert('Enter value from 0 to 100');
+        alert('Enter value from 0-100');
     }
 };
