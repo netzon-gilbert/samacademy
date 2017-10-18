453ff85f244533cd1eb564be4bab721c973e1e15
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-18 18:11:37 +0800

    vue exercises: js exercise 4 find the second minimum and maximum numbers in the array

--- js/exercises/Exer 4/exer.js
+++ js/exercises/Exer 4/exer.js
@@ -1,14 +1,14 @@
-// TODO: Write your solution here
-function secMaxima(nput) {
-
-    nput.sort(function (a, b) {return a - b});
-    return '(' + nput[1] + ', ' + nput[nput.length - 2] +')';
-};
-
-function run() {
-    // TODO: Start calling your function here
-    var output = document.getElementById('output');
-        output.innerHTML = secMaxima([1,4,9,3,35,8]);
-
-    console.log('Code Awesome');
-};
+new Vue({
+    el : '.app',
+    data : {
+        nput : [1, 4, 9, 3, 35, 8],
+        out : ''
+    },
+    methods : {
+        secMaxima : function () {
+            var nput = this.nput;
+            nput.sort(function (a, b) {return a - b});
+            this.out = '(' + nput[1] + ', ' + nput[nput.length - 2] +')';
+        }
+    }
+});
