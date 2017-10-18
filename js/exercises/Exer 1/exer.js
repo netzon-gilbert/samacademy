f5c39208852709e1ee49e40f8e927c3e931a056b
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-18 15:09:14 +0800

    vue exercises: js exercise 1 finish

--- js/exercises/Exer 1/exer.js
+++ js/exercises/Exer 1/exer.js
@@ -1,17 +1,6 @@
-// TODO: Write your solution here
-function reverseNum(nput) {
-
-    var numString = new String(nput);
-    return numString.split('').reverse().join('');
-
-};
-
-function run() {
-    // TODO: Start calling your function here
-    var input = document.getElementById('input').value,
-        output = document.getElementById('output');
-
-        output.innerHTML = reverseNum(input);
-
-    console.log('Code Awesome');
-};
+new Vue({
+    el : '.app',
+    data : {
+        nput : ''
+    }
+});
