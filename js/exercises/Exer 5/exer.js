023d4e0e86c59e67697f0ab0c9be22bd334edcd6
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-18 18:30:49 +0800

    vue exercises: js exercise 5 out b to power of n

--- js/exercises/Exer 5/exer.js
+++ js/exercises/Exer 5/exer.js
@@ -1,11 +1,15 @@
-// TODO: Write your solution here out;
-function run() {
-    // TODO: Start calling your function here
-    var input1 = document.getElementById('input1').value,
-        input2 = document.getElementById('input2').value,
-        output = document.getElementById('output');
-
-        output.innerHTML = Math.pow(input1, input2);
-
-    console.log('Code Awesome');
-};
+new Vue({
+    el : '.app',
+    data : {
+        nput1 : '',
+        nput2 : '',
+        out : ''
+    },
+    methods : {
+        power : function () {
+            var b = parseInt(this.nput1),
+                n = parseInt(this.nput2);
+            this.out = Math.pow(b, n);
+        }
+    }
+});
