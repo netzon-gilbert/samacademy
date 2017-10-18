583aba5b518c3173d70139f6afdcda18b67a5055
gilbert cuerbo <netzon.gilbert@gmail.com>
2017-10-18 17:58:54 +0800

    vue exercises: js exercise 3 done funtion that tells if the number is prime or not

--- js/exercises/Exer 3/exer.js
+++ js/exercises/Exer 3/exer.js
@@ -1,32 +1,30 @@
-// TODO: Write your solution here
-function isPrime(nput) {
+new Vue({
+    el : '.app',
+    data : {
+        nput : '',
+        out : ''
+    },
+    methods : {
+        isPrime : function () {
+            var out = 'no entry',
+                nput = parseInt(this.nput);
 
-    var out = 'no entry';
+            if (nput == 1) {
+                out = 'prime';
+            } else if (nput == 2) {
+                out = 'not prime';
+            }
 
-    if (nput == 1) {
-        out = 'prime';
-    } else if (nput == 2) {
-        out = 'not prime';
-    }
+            for (var i = 2; i < nput; i++) {
+                if (nput % i == 0) {
+                    out = 'not prime';
+                    break;
+                } else {
+                    out = 'prime';
+                }
+            }
 
-    for (var i = 2; i < nput; i++) {
-        if (nput % i == 0) {
-            out = 'not prime';
-            break;
-        } else {
-            out = 'prime';
+            this.out = out;
         }
     }
-
-    return out;
-};
-
-function run() {
-    // TODO: Start calling your function here
-    var input = document.getElementById('input').value,
-        output = document.getElementById('output');
-
-        output.innerHTML = isPrime(input);
-
-    console.log('Code Awesome');
-};
+});
