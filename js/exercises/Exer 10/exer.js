14a1a623df61a50716dcd2e360056ca9318fe166
netzon.gilbert <gilbert.cuerbo@gmail.com>
2017-10-23 17:39:08 +0800

    vue testing: exercise 10, listing the propeties from an object

--- js/exercises/Exer 10/exer.js
+++ js/exercises/Exer 10/exer.js
@@ -1,23 +1,10 @@
-// TODO: Write your solution here
-var student = {
-        name : 'David Rayy',
-        sclass : 'VI',
-        rollno : 12
-    };
-
-function listProp(){
-
-    var out = '<ul>';
-    for (var count in student) {
-        out += '<li>' + count + ' = ' + student[count] + '</li>';
+var vue = new Vue({
+    el : '.app',
+    data : {
+        student : [
+            {info : 'David Rayy'},
+            {info : 'VI'},
+            {info : 12}
+        ]
     }
-
-    out += '</ul>';
-    return out;
-};
-
-function run () {
-    // TODO: Start calling your function here
-    document.getElementById('output').innerHTML = listProp();
-    console.log('Code Awesome');
-};
+});
