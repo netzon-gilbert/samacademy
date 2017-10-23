878bb46ac4d80569b2218077917ad22ac73a6215
gilbert cuerbo <gilbert.cuerbo@gmail.com>
2017-10-23 21:30:20 +0800

    vue testing: exercises 14, finding the first none repeated character

--- js/exercises/Exer 14/exer.js
+++ js/exercises/Exer 14/exer.js
@@ -1,47 +1,49 @@
-// TODO: Write your solution here
-function countRepeat(nput, string) {
+new Vue({
+    el : '.app',
+    data : {
+        out : '',
+        nput : ''
+    },
+    methods : {
+        countRepeat : function (nput, string) {
 
-    var newString = string.toLowerCase(),
-        count = 0;
+            var newString = string.toLowerCase(),
+                count = 0;
 
-    for (var i = 0; i < newString.length; i++) {
-        if (newString.charAt(i) == nput) {
-            count++;
+            for (var i = 0; i < newString.length; i++) {
+                if (newString.charAt(i) == nput) {
+                    count++;
+                }
+            }
+
+            return count;
+        },
+        countInString : function (string) {
+
+            var stringNum = new Array();
+            for (var i = 0; i < string.length; i++) {
+                stringNum[i] = 0;
+                stringNum[i] = this.countRepeat(string.charAt(i), string);
+            }
+
+            return stringNum;
+        },
+        firstNoneRepeat : function (nput){
+
+            var numArray = this.countInString(nput),
+                char;
+
+            for (var i = 0; i < numArray.length; i++) {
+                if (numArray[i] == 1) {
+                    char = nput.charAt(i);
+                    break;
+                }
+            }
+
+            return char;
+        },
+        run : function () {       
+            this.out = this.firstNoneRepeat(this.nput);
         }
     }
-
-    return count;
-};
-
-function countInString(string) {
-
-    var stringNum = new Array();
-    for (var i = 0; i < string.length; i++) {
-        stringNum[i] = 0;
-        stringNum[i] = countRepeat(string.charAt(i), string);
-    }
-
-    return stringNum;
-};
-
-function firstNoneRepeat(nput){
-
-    var numArray = countInString(nput),
-        char;
-
-    for (var i = 0; i < numArray.length; i++) {
-        if (numArray[i] == 1) {
-            char = nput.charAt(i);
-            break;
-        }
-    }
-
-    return char;
-};
-
-function run () {
-    // TODO: Start calling your function here
-    var input = document.getElementById('input').value;
-    document.getElementById('output').innerHTML = firstNoneRepeat(input);
-    console.log('Code Awesome');
-};
+});
