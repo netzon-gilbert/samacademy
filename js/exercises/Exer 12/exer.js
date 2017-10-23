03021e48b61ba152c8d4732c38e44bc6157edf0d
netzon.gilbert <gilbert.cuerbo@gmail.com>
2017-10-23 18:11:14 +0800

    vue testing: exercise 12, create a clock

--- js/exercises/Exer 12/exer.js
+++ js/exercises/Exer 12/exer.js
@@ -1,26 +1,34 @@
-// TODO: Write your solution here
-function getTime() {
+var vue = new Vue({
+    el : '.app',
+    data : {
+        hr : 0,
+        mn : 0,
+        sc : 0,
+        dy : 'AM'
+    },
+    methods : {
+        getTime : function () {
+            var time = new Date();
+            var h = time.getHours();
+            var m = time.getMinutes();
+            var s = time.getSeconds();
+            var day = 'AM';
 
-    var time = new Date();
-    var h = time.getHours();
-    var m = time.getMinutes();
-    var s = time.getSeconds();
-    var day = 'AM';
+            if (h > 12) {
+                h = h - 12;
+                day = 'PM';
+            }
 
-    if (h > 12) {
-        h = h - 12;
-        day = 'PM';
+            this.hr = h;
+            this.mn = m;
+            this.sc = s;
+            this.dy = day;
+        },
+        init : function () {
+            var self = this;
+            setInterval(function () {
+                self.getTime();
+            }, 300);
+        }
     }
-
-    return h + ' :' + m + ': ' +s +' ' + day;
-};
-
-function run() {
-    // TODO: Start calling your function here
-    document.getElementById('output').innerHTML = getTime();
-    setTimeout(function () {
-        run();
-    }, 300);
-
-    console.log('Code Awesome');
-};
+});
